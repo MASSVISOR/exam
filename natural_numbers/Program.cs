83cf5173565cbdf88da2398bda14a0c5ba6f70bc
Kosta <cherniysmm@gmail.com>
2024-04-25 18:11:48 +0200

    create new console

---
+++ natural_numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+Console.WriteLine("Введите значение M:");
+int m = int.Parse(Console.ReadLine());
+        
+Console.WriteLine("Введите значение N:");
+int n = int.Parse(Console.ReadLine());
+        
+Console.WriteLine($"Натуральные числа от m до n:");
+PrintNaturalNumbers(m, n);
+
+
+static void PrintNaturalNumbers(int m, int n)
+
+    if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNaturalNumbers(m + 1, n);
+        }
