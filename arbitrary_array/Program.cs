c531fe64c7180e91beb3d43e5dcb6fc971700626
Kosta <cherniysmm@gmail.com>
2024-04-25 18:57:37 +0200

    finish commit

--- arbitrary_array/Program.cs
+++ arbitrary_array/Program.cs
@@ -5,8 +5,8 @@ public class Program
 {
     static void Main(string[] args)
     {
-        int[] array = { 0, 1, 2, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15 };
-        Console.WriteLine("Выводим его элементы, начиная с конца:");
+        int[] array = { 0, 1, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        Console.WriteLine("Выводим элементы массива, начиная с конца:");
         PrintArrayReverse(array, array.Length - 1);
     }
 
