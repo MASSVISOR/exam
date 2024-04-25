ccae5435c76f7dcd3797f0399805038d5ec7e8c1
Kosta <cherniysmm@gmail.com>
2024-04-25 18:02:42 +0200

    changes in working directory

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -1,28 +1,29 @@
 ﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-using System;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Введите натуральное число n:");
+        Console.WriteLine("Введите натуральное число m:");
         int m = Convert.ToInt32(Console.ReadLine());
-        
+
         Console.WriteLine("Введите натуральное число n:");
         int n = Convert.ToInt32(Console.ReadLine());
-        
-        int result = AckermannFunction(m, n);
-        Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}: {result}");
-    }
 
-    static int AckermannFunction(int m, int n)
-    {
-        if (m == 0)
-            return n + 1;
-        else if (n == 0)
-            return AckermannFunction(m - 1, 1);
-        else
-            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        int result = AckermannFunction(m, n);
+        Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}");
+        Console.WriteLine(result);
+
+
+        static int AckermannFunction(int m, int n)
+        {
+            if (m == 0)
+                return n + 1;
+            else if (n == 0)
+                return AckermannFunction(m - 1, 1);
+            else
+                return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
     }
 }
